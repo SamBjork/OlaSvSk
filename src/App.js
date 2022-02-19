8eb93d7e1b8cad9f7a9248c7075cdbd6d3a6aafa
Sam <samuel.bjorkqvist@gmail.com>
2022-02-19 11:43:05 +0100

    name changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Parallax, Background } from "react-parallax";
+import sthlm from "./assets/1093970.jpg";
+
+const Container = () => (
+  <Parallax strength={300}>
+    <Background className="custom-bg">
+      <img src={sthlm} alt="fill murray" />
+    </Background>
+  </Parallax>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +26,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <Container className="Hero" />
+      <footer className="Footer"></footer>
     </div>
   );
 }
